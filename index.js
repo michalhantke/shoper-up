4f82f9b02a65c05bf40a373d5089c42841fc2e5c
michalhantke <michal.hantke@shoper.pl>
2022-06-06 21:55:49 +0200

    zmodzenie index.html i dodanie funkcji w js

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+let dayOfTheWeek = 6;
+if (dayOfTheWeek == 1) {
+    console.log("Poniedziałek")
+} else if (dayOfTheWeek == 2) {
+    console.log("Wtorek")
+} else if (dayOfTheWeek == 3) {
+    console.log("Środa")
+} else if (dayOfTheWeek == 4) {
+    console.log("Czwartek")
+} else if (dayOfTheWeek == 5) {
+    console.log("Piątek")
+} else if (dayOfTheWeek == 6) {
+    console.log("Sobota")
+} else if (dayOfTheWeek == 7) {
+    console.log("Niedziela")
+} else {console.log("niepoprawna wartość")}
